44b66a282e50242b071737e8c6534c040a98e0f1
missmarcus <lise.marcus@gmail.com>
2018-06-07 14:41:17 +0200

    added a network :)

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var graph = Viva.Graph.graph();
+        graph.addLink(1, 2,);
+        graph.addLink(2, 3,);
+        graph.addLink(3, 4,);
+        graph.addLink(1, 4,);
+        
+        // specify where it should be rendered:
+        var renderer = Viva.Graph.View.renderer(graph, {
+          container: document.getElementById('graphDiv')
+        });
+        renderer.run();
