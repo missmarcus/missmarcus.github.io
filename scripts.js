d55a19dd61a78aed7338f2714e3e8b9c248abfd2
missmarcus <lise.marcus@gmail.com>
2018-06-07 14:46:03 +0200

    data

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ var graph = Viva.Graph.graph();
         graph.addLink(2, 3,);
         graph.addLink(3, 4,);
         graph.addLink(1, 4,);
+        graph.addLink(2, 4,);
+        graph.addLink(1, 3,);
         
         // specify where it should be rendered:
         var renderer = Viva.Graph.View.renderer(graph, {
